1a13d0d8a8f489c6d202bbecc0f6ed77a16adcc4
Don Roche <donrochemedia@gmail.com>
2021-06-09 11:15:44 +0100

    inputs

--- daft.py
+++ daft.py
@@ -5,3 +5,13 @@ from daft_scraper.search.options import (
 )
 from daft_scraper.search.options_location import LocationsOption, Location
 import csv 
+
+print("Hello CCT College student. I do not wish to be rude, but may I ask what you maximum rent should be?\n" +   
+            "I assume as a sttudent there is no minimum rent.")
+    maxRent = input("Max rent: ")
+
+    print("Splendid. Would you like a house or an apartment?")
+    propType = input("h or a?: ")
+
+    print("Finally, one lump or two? How many bedrooms? Must be numerical.")
+    numBedrooms = int(input("Bedrooms 1-10: "))
